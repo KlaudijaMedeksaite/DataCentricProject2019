8aefab4306fe63415e72a26d7dd0283936025718
Klaudija Medeksaite <g00353761@gmit.ie>
2019-12-08 12:04:01 +0000

    Start of project, product addition working

---
+++ G00353761/src/com/shops/Product.java
@@ -0,0 +1,38 @@
+package com.shops;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Product {
+
+	int pid;
+	int sid;
+	String prodName;
+	double price;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
